e40625c0b2de71e5d9934932735475edb23474ea
soltris <soltris@users.noreply.github.com>
2025-04-03 18:07:54 -0400

    Classify LabelRow and DemonSpy
    Made Application and LabelRow separate classes and added amulet and monster info as labels. Also added styling and ThemedTk package.

--- DemonSpy.py
+++ DemonSpy.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from ttkthemes import ThemedTk
 
 from ReadWriteMemory import ReadWriteMemory
 
@@ -13,7 +14,7 @@ P1_SPEED_TIMER_OFFSET = 0x284ad
 P1_INVIS_TIMER_OFFSET = 0x284ae
 P1_WALLWALK_TIMER_OFFSET = 0x284af
 P1_PARALYSIS_TIMER_OFFSET = 0x284b0
-MON1_DMG_OFFSET = 0x28cf2
+MON1_DMG_OFFSET = 0x28c42
 MON1_HP_OFFSET = 0x28c43
 MON2_DMG_OFFSET = 0x28c46
 MON2_HP_OFFSET = 0x28c47
@@ -43,155 +44,132 @@ def joinBytes(size: int, j: list[hex]) -> int:
 		total += value * 256 ** i
 	return total
 
-def updateLabel(label, gameProcess, inputPointer, size: int):
-	print(gameProcess)
-	print(inputPointer)
-	print(size)
-	print('readValue:',readValue(gameProcess,inputPointer, size))
-	label.set(readValue(gameProcess, inputPointer, size))
+# This class makes a row of labels
+# Inputs are the parent to the frame and a list of labels to make a row out of
+class LabelRow(ttk.Frame):
+	def __init__(self, parent, labels, row, column, sticky):
+		super().__init__(parent)
+		self.labels=[]
+		for i in range(len(labels)):
+			if isinstance(labels[i], str):
+				self.labels.append(tk.Label(self, text=labels[i]))
+			else:
+				self.labels.append(tk.Label(self, textvariable=labels[i]))
+			self.labels[i].grid(row=0, column=i, sticky=sticky)
+			self.labels[i].configure(font = ("Helvetica", 16, "bold"))
+			self.columnconfigure(i, weight=1)
 
-class Application(tk.Tk):
+		self.grid(row=row, column=column, sticky=sticky, padx=5)
+
+class Application(ThemedTk):
+
 	def __init__(self):
-		super().__init__()
+		super().__init__(theme="Adapta")
 		self.title("Demon Spy")
 		self.columnconfigure(0, weight=1)
-		self.rowconfigure(0, weight=1)
-		self.rowconfigure(1, weight=1)
-		self.rowconfigure(2, weight=1)
-		self.rowconfigure(3, weight=1)
-		self.rowconfigure(4, weight=1)
+		for i in range(13):
+			self.rowconfigure(i, weight=1)
 
-		playerStatLabelList = ["Magic", "Strength", "Defense"]
-		playerStatVariableList = ["Magic", "Strength", "Defense"]
-		requirementCategoryList = ["Required Items"]
-		requirementLabelList = ["Scrolls", "Amulets", "Artifacts", "Relics", "Chests"]
-		requirementVariableList = ["Scrolls", "Amulets", "Artifacts", "Relics", "Chests"]
-
-		# Set up frame containers
-		playerStatLabelFrame = ttk.Frame(self)
-		playerStatLabelFrame.grid(row=0, column=0, sticky="SEW", padx=5)
-		playerStatLabelFrame.columnconfigure(0, weight=1)
-		playerStatLabelFrame.columnconfigure(1, weight=1)
-		playerStatLabelFrame.columnconfigure(2, weight=1)
-		playerStatFrame = ttk.Frame(self)
-		playerStatFrame.grid(row=1, column=0, sticky="NEW", padx=5)
-		playerStatFrame.columnconfigure(0, weight=1)
-		playerStatFrame.columnconfigure(1, weight=1)
-		playerStatFrame.columnconfigure(2, weight=1)
-		requirementCategoryFrame = ttk.Frame(self)
-		requirementCategoryFrame.grid(row=2, column=0, sticky="EW", padx=5, pady=5)
-		requirementCategoryFrame.columnconfigure(0, weight=1)
-		requirementLabelFrame = ttk.Frame(self)
-		requirementLabelFrame.grid(row=3, column=0, sticky="SEW", padx=5)
-		requirementLabelFrame.columnconfigure(0, weight=1)
-		requirementLabelFrame.columnconfigure(1, weight=1)
-		requirementLabelFrame.columnconfigure(2, weight=1)
-		requirementLabelFrame.columnconfigure(3, weight=1)
-		requirementLabelFrame.columnconfigure(4, weight=1)
-		requirementFrame = ttk.Frame(self)
-		requirementFrame.grid(row=4, column=0, sticky="NEW", padx=5)
-		requirementFrame.columnconfigure(0, weight=1)
-		requirementFrame.columnconfigure(1, weight=1)
-		requirementFrame.columnconfigure(2, weight=1)
-		requirementFrame.columnconfigure(3, weight=1)
-		requirementFrame.columnconfigure(4, weight=1)
-
+		# Player stat variables
 		playerHealth = tk.IntVar()
 		playerScore = tk.IntVar()
 		playerMagic = tk.IntVar()
 		playerStrength = tk.IntVar()
 		playerDefense = tk.IntVar()
+
+		# Variables for required items - All must be 0 to exit level
 		scrollsLeft = tk.IntVar()
 		amuletsLeft = tk.IntVar()
 		artifactsLeft = tk.IntVar()
 		relicsLeft = tk.IntVar()
 		chestsLeft = tk.IntVar()
 
+		# Amulet timers, expressed in seconds
+		speedTimer = tk.IntVar()
+		invisTimer = tk.IntVar()
+		wallwalkTimer = tk.IntVar()
+		paralysisTimer = tk.IntVar()
+
+		# Monster variables
+		monsterOneHP = tk.IntVar()
+		monsterOneDamage = tk.IntVar()
+		monsterTwoHP = tk.IntVar()
+		monsterTwoDamage = tk.IntVar()
+
 		rwm = ReadWriteMemory()
 
-		process = rwm.get_process_by_name('DOSBox.exe')
-		process.open()
-		base_address = process.get_base_address()
+		# Find base DOSBox process location in RAM
+		self.process = rwm.get_process_by_name('DOSBox.exe')
+		self.process.open()
+		base_address = self.process.get_base_address()
 
-		# Get pointers for basic player values after finding game's base memory address
-		healthPointer = process.get_pointer(base_address+GAME_OFFSET, offsets=[HEALTH_OFFSET])
-		scorePointer = process.get_pointer(base_address+GAME_OFFSET, offsets=[SCORE_OFFSET])
-		magicPointer = process.get_pointer(base_address+GAME_OFFSET, offsets=[MAG_OFFSET])
-		strengthPointer = process.get_pointer(base_address+GAME_OFFSET, offsets=[STR_OFFSET])
-		defensePointer = process.get_pointer(base_address+GAME_OFFSET, offsets=[DEF_OFFSET])
-		scrollsLeftPointer = process.get_pointer(base_address+GAME_OFFSET, offsets=[SCROLL_REQ_OFFSET])
-		amuletsLeftPointer = process.get_pointer(base_address+GAME_OFFSET, offsets=[AMULET_REQ_OFFSET])
-		artifactsLeftPointer = process.get_pointer(base_address+GAME_OFFSET, offsets=[ARTIFACT_REQ_OFFSET])
-		relicsLeftPointer = process.get_pointer(base_address+GAME_OFFSET, offsets=[RELIC_REQ_OFFSET])
-		chestsLeftPointer = process.get_pointer(base_address+GAME_OFFSET, offsets=[CHEST_REQ_OFFSET])
+		# Because Demon Stalkers always loads in the same spot in memory relative to DOSBox on a clean boot, it too has an offset
+		# We calculate all memory pointers from these static offsets after finding DOSBox
+		healthPointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[HEALTH_OFFSET])
+		scorePointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[SCORE_OFFSET])
+		magicPointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[MAG_OFFSET])
+		strengthPointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[STR_OFFSET])
+		defensePointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[DEF_OFFSET])
+		scrollsLeftPointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[SCROLL_REQ_OFFSET])
+		amuletsLeftPointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[AMULET_REQ_OFFSET])
+		artifactsLeftPointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[ARTIFACT_REQ_OFFSET])
+		relicsLeftPointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[RELIC_REQ_OFFSET])
+		chestsLeftPointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[CHEST_REQ_OFFSET])
+		speedTimerPointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[P1_SPEED_TIMER_OFFSET])
+		invisTimerPointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[P1_INVIS_TIMER_OFFSET])
+		wallwalkTimerPointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[P1_WALLWALK_TIMER_OFFSET])
+		paralysisTimerPointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[P1_PARALYSIS_TIMER_OFFSET])
+		monsterOneHPPointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[MON1_HP_OFFSET])
+		monsterOneDamagePointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[MON1_DMG_OFFSET])
+		monsterTwoHPPointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[MON2_HP_OFFSET])
+		monsterTwoDamagePointer = self.process.get_pointer(base_address+GAME_OFFSET, offsets=[MON2_DMG_OFFSET])
 
-		# Update labels with all sought-for values
-		playerHealth.set(readValue(process, healthPointer, 2))
-		playerScore.set(readValue(process, scorePointer, 2))
-		playerMagic.set(readValue(process, magicPointer, 1))
-		playerStrength.set(readValue(process, strengthPointer, 1))
-		playerDefense.set(readValue(process, defensePointer, 1))
-		scrollsLeft.set(readValue(process, scrollsLeftPointer, 1))
-		amuletsLeft.set(readValue(process, amuletsLeftPointer, 1))
-		artifactsLeft.set(readValue(process, artifactsLeftPointer, 1))
-		relicsLeft.set(readValue(process, relicsLeftPointer, 1))
-		chestsLeft.set(readValue(process, chestsLeftPointer, 1))
+		# Set up lists of variables to iterate through
+		self.dataOffsets = [MAG_OFFSET, STR_OFFSET, DEF_OFFSET, SCROLL_REQ_OFFSET, AMULET_REQ_OFFSET, ARTIFACT_REQ_OFFSET, RELIC_REQ_OFFSET, CHEST_REQ_OFFSET, P1_SPEED_TIMER_OFFSET, P1_INVIS_TIMER_OFFSET, P1_WALLWALK_TIMER_OFFSET, P1_PARALYSIS_TIMER_OFFSET, MON1_HP_OFFSET, MON1_DMG_OFFSET, MON2_HP_OFFSET, MON2_DMG_OFFSET]
+		self.dataPointers = [magicPointer, strengthPointer, defensePointer, scrollsLeftPointer, amuletsLeftPointer, artifactsLeftPointer, relicsLeftPointer, chestsLeftPointer, speedTimerPointer, invisTimerPointer, wallwalkTimerPointer, paralysisTimerPointer, monsterOneHPPointer, monsterOneDamagePointer, monsterTwoHPPointer, monsterTwoDamagePointer]
+		self.dataVariables = [playerMagic, playerStrength, playerDefense, scrollsLeft, amuletsLeft, artifactsLeft, relicsLeft, chestsLeft, speedTimer, invisTimer, wallwalkTimer, paralysisTimer, monsterOneHP, monsterOneDamage, monsterTwoHP, monsterTwoDamage]
 
-		# Create all the Labels for the main layout
-		magicStatLabel = tk.Label(playerStatLabelFrame, text='  Magic')
-		strengthStatLabel = tk.Label(playerStatLabelFrame, text='  Strength')
-		defenseStatLabel = tk.Label(playerStatLabelFrame, text=' Defense')
-		magicStat = tk.Label(playerStatFrame, textvariable=playerMagic)
-		strengthStat = tk.Label(playerStatFrame, textvariable=playerStrength)
-		defenseStat = tk.Label(playerStatFrame, textvariable=playerDefense)
-		requirementLabel = tk.Label(requirementCategoryFrame,text='Required Items')
-		scrollReqLabel = tk.Label(requirementLabelFrame,text='Scrolls')
-		scrollReq = tk.Label(requirementFrame, text=scrollsLeft.get())
-		amuletReqLabel = tk.Label(requirementLabelFrame,text='Amulets')
-		amuletReq = tk.Label(requirementFrame, text=amuletsLeft.get())
-		artifactReqLabel = tk.Label(requirementLabelFrame,text='Artifacts')
-		artifactReq = tk.Label(requirementFrame, text=artifactsLeft.get())
-		relicReqLabel = tk.Label(requirementLabelFrame,text='Relics')
-		relicReq = tk.Label(requirementFrame,text=relicsLeft.get())
-		chestReqLabel = tk.Label(requirementLabelFrame,text='Chests')
-		chestReq = tk.Label(requirementFrame,text=chestsLeft.get())
+		self.after(250, self.updateLabel)
 
-		## Layout of main window
-	
-		# Row 1: Player stat labels
-		magicStatLabel.grid(row=0, column=0)
-		strengthStatLabel.grid(row=0, column=1)
-		defenseStatLabel.grid(row=0, column=2)
-		
-		# Row 2: Player stats
-		magicStat.grid(row=0, column=0)
-		strengthStat.grid(row=0, column=1)
-		defenseStat.grid(row=0, column=2)
-	
-		# Row 3: Top Required Items Label
-		requirementLabel.grid(row=0, column=0)
-		
-		# Row 4: Required Items Category Labels
-		scrollReqLabel.grid(row=0, column=0)
-		amuletReqLabel.grid(row=0, column=1)
-		artifactReqLabel.grid(row=0, column=2)
-		relicReqLabel.grid(row=0, column=3)
-		chestReqLabel.grid(row=0, column=4)
-	
-		# Row 4: Requirements
-		scrollReq.grid(row=0, column=0)
-		amuletReq.grid(row=0, column=1)
-		artifactReq.grid(row=0, column=2)
-		relicReq.grid(row=0, column=3)	
-		chestReq.grid(row=0, column=4)
+		# Initialize lists of label titles and variables, iterated through for FrameRows
+		self.playerStatLabelList = ["Magic", "Strength", "Defense"]
+		self.playerStatVariableList = [playerMagic, playerStrength, playerDefense]
+		self.requirementCategoryList = ["Required Items"]
+		self.requirementLabelList = ["Scrolls", "Amulets", "Artifacts", "Relics", "Chests"]
+		self.requirementVariableList = [scrollsLeft, amuletsLeft, artifactsLeft, relicsLeft, chestsLeft]
+		self.amuletCategoryList = ["Amulet Timers"]
+		self.amuletLabelList = ["Speed", "Invisibility", "Wallwalk", "Paralysis"]
+		self.amuletVariableList = [speedTimer, invisTimer, wallwalkTimer, paralysisTimer]
+		self.monsterLabelList = ["Monster 1", "Monster 2"]
+		self.monsterHPList = ["HP", monsterOneHP, "HP", monsterTwoHP]
+		self.monsterDamageList = ["Damage", monsterOneDamage, "Damage", monsterTwoDamage]
 
-class LabelRow(ttk.Frame):
-	def __init__(self, parent, labelList):
-		super().__init__(parent)
-		for i in range(labelList):
-			self.columnconfigure(i, weight=1)
+		# Set layout and make rows of labels
+		# Arguments: Parent window, list of labels, row, column, sticky)
+		self.playerStatLabelRow = LabelRow(self, self.playerStatLabelList, 0, 0, "SEW")
+		self.playerStatVariableRow = LabelRow(self, self.playerStatVariableList, 1, 0, "NEW")
+		self.playerStatSeparator = ttk.Separator(self, orient="horizontal")
+		self.playerStatSeparator.grid(row=2, column=0, sticky="EW")
+		self.requirementCategoryRow = LabelRow(self, self.requirementCategoryList, 3, 0, "EW")		
+		self.requirementLabelRow = LabelRow(self, self.requirementLabelList, 4, 0, "SEW")
+		self.requirementVariableRow = LabelRow(self, self.requirementVariableList, 5, 0, "NEW")
+		self.requirementSeparator = ttk.Separator(self, orient="horizontal")
+		self.requirementSeparator.grid(row=6, column=0, sticky="EW")
+		self.amuletCategoryRow = LabelRow(self, self.amuletCategoryList, 7, 0, "EW")
+		self.amuletLabelRow = LabelRow(self, self.amuletLabelList, 8, 0, "SEW")
+		self.amuletVariableRow = LabelRow(self, self.amuletVariableList, 9, 0, "NEW")
+		self.amuletSeparator = ttk.Separator(self, orient="horizontal")
+		self.amuletSeparator.grid(row=10, column=0, sticky="EW")
+		self.monsterLabelRow = LabelRow(self, self.monsterLabelList, 11, 0, "SEW")
+		self.monsterHPRow = LabelRow(self, self.monsterHPList, 12, 0, "EW")
+		self.monsterDamageRow = LabelRow(self, self.monsterDamageList, 13, 0, "NEW")
 
-#statList = ['magic','strength','defense','scrollsLeft','amuletsLeft','artifactsLeft','relicsLeft','chestsLeft']
+
+	def updateLabel(self):
+		if len(self.dataVariables) == len(self.dataPointers):
+			for i in range(len(self.dataPointers)):
+				self.dataVariables[i].set(readValue(self.process, self.dataPointers[i], 1))
+		self.after(250, self.updateLabel)
 
 if __name__ == "__main__":
 	main()
